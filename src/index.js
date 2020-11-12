43a03bca09bd9f3d7fab702dd34f2a228a6fbd5c
Jeferson Andrés S <jeffersonmx@outlook.com>
2020-11-12 03:50:48 -0500

    Adición de mensaje embed y más reacciones

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const messageEmbed = require('./utils/messageEmbed');
 
 require('dotenv').config();
+
+const rules = require('./messages/rules.json');
+const url = require('./messages/url.json');
 
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
 client.on('message', msg => {
     console.log(msg.content);
     if (msg.content.toLocaleLowerCase() === 'hola') {
-      msg.reply('Hola');
-      msg.reply(':sunglasses:');
+      msg.reply('Hola brrrrr');
+      msg.react('😎');
+    }
+    else if (msg.content.toLocaleLowerCase() === '!reglas') {
+      const author = msg.author.username
+      //msg.channel.send(`Estas son las reglas, ${author}`)
+      msg.channel.send(messageEmbed({
+        authorName: author,
+        title: rules.customMessage,
+        content: rules.rules
+      }))
+    }
+    else if (msg.content.toLocaleLowerCase() === 'adios') {
+      msg.reply('shu, ve a dormir pto');
+      msg.channel.send(url.url);
     }
 });
 
 client.on('ready', ()=>{
     console.log(`Conectado... ${client.user.tag}`);
 });
+
+
 
 client.login(DISCORD_TOKEN);
